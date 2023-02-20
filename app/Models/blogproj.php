d968d652651d9ceea6daf9bf51395dfc7463b054
ivanrahano888 <ivanrahano2018@gmail.com>
2023-02-20 17:38:04 +0800

    dispatching the event

--- app/Models/blogproj.php
+++ app/Models/blogproj.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\BlogprojCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -15,6 +16,9 @@ class blogproj extends Model
         'message',
     ];
 
+    protected $dispatchesEvents = [
+        'created' => BlogprojCreated::class,
+    ];
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
